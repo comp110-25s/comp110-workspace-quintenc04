859f149ee7a912a1149a526cac185a4f91c3e70f
quintenc04 <quintenc@unc.edu>
2025-03-03 00:43:57 -0500

    exercise 2

--- exercises/ex02_wordle.py
+++ exercises/ex02_wordle.py
@@ -15,18 +15,21 @@ def main(secret: str) -> None:
         input = input_guess(len(secret))
         print(emojified(input, secret))
         if input == secret:
-            print(f"You won in {i} turns!")
+            print(f"You won in {i}/6 turns!")
+            return
         else:
             i += 1
-        if i >= 7:
+        if i > 6:
             print("X/6 - Sorry, try again tomorrow!")
 
 
 def contains_char(word: str, letter: str) -> bool:
     """determines if the secret word contains the guessed character"""
     assert len(letter) == 1, f"len('{letter}') is not 1"
-    i: int = 0
-    while i < len(word):
+    i: int = 0  # recursion base case
+    while i < len(
+        word
+    ):  # loops through each index in word str to check if the guessed letter is in it
         if word[i] == letter:
             return True
         i += 1  # relative reassignment operator
@@ -36,23 +39,33 @@ def contains_char(word: str, letter: str) -> bool:
 def emojified(guess: str, secret_word: str) -> str:
     """Uses contains_char to test if guess letter is in secret word/correct position"""
     assert len(guess) == len(secret_word), "Guess must be same length as secret"
-    emojis: str = ""
-    i: int = 0
-    while i < len(guess):
-        if contains_char(secret_word, guess[i]) is False:
+    emojis: str = ""  # an empty string to add the emojis to
+    i: int = 0  # recursion base case
+    while i < len(
+        guess
+    ):  # loops through each index in guess string to build a string of emojis
+        if (
+            contains_char(secret_word, guess[i]) is False
+        ):  # current index of guess str matches no secret letters; white box returned
             emojis += WHITE_BOX
-        elif secret_word[i] == guess[i]:
+        elif (
+            secret_word[i] == guess[i]
+        ):  # if str at guess idx = str at secret word idx, guessed letter in right spot
             emojis += GREEN_BOX
         else:
             emojis += YELLOW_BOX
         i += 1
-    return emojis
+    return emojis  # returns fully buidl emojis string with len(guess) boxes
 
 
 def input_guess(expected_length: int) -> str:
     """prompts user for guess until expected length is input"""
-    response = input(f"Enter a {expected_length} character word: ")
-    while len(response) != expected_length:
+    response = input(
+        f"Enter a {expected_length} character word: "
+    )  # prompts user to input a response and assigns that response to a variable
+    while (
+        len(response) != expected_length
+    ):  # continuously asks user for another input as long as the lengths do not match
         response = input(f"That wasn't {expected_length} chars! Try again: ")
     return response
 
