2d42404637a5e13343f1fee74a7473203b201636
quintenc04 <quintenc@unc.edu>
2025-03-02 23:51:05 -0500

    exercise 2

--- exercises/ex02_wordle.py
+++ exercises/ex02_wordle.py
@@ -7,6 +7,21 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 
+def main(secret: str) -> None:
+    """The entrypoint of the program and main game loop."""
+    i = 1
+    while i <= 6:
+        print(f"=== Turn {i}/6 ===")
+        input = input_guess(len(secret))
+        print(emojified(input, secret))
+        if input == secret:
+            print(f"You won in {i} turns!")
+        else:
+            i += 1
+        if i >= 7:
+            print("X/6 - Sorry, try again tomorrow!")
+
+
 def contains_char(word: str, letter: str) -> bool:
     """determines if the secret word contains the guessed character"""
     assert len(letter) == 1, f"len('{letter}') is not 1"
@@ -18,15 +33,15 @@ def contains_char(word: str, letter: str) -> bool:
     return False
 
 
-def emojified(guess: str, secret: str) -> str:
+def emojified(guess: str, secret_word: str) -> str:
     """Uses contains_char to test if guess letter is in secret word/correct position"""
-    assert len(guess) == len(secret), "Guess must be same length as secret"
+    assert len(guess) == len(secret_word), "Guess must be same length as secret"
     emojis: str = ""
     i: int = 0
     while i < len(guess):
-        if contains_char(secret, guess[i]) is False:
+        if contains_char(secret_word, guess[i]) is False:
             emojis += WHITE_BOX
-        elif secret[i] == guess[i]:
+        elif secret_word[i] == guess[i]:
             emojis += GREEN_BOX
         else:
             emojis += YELLOW_BOX
@@ -37,8 +52,10 @@ def emojified(guess: str, secret: str) -> str:
 def input_guess(expected_length: int) -> str:
     """prompts user for guess until expected length is input"""
     response = input(f"Enter a {expected_length} character word: ")
-    if len(response) == expected_length:
-        return "response"
-    else:
-        response = input("That wasn't {expected_length} chars! Try again: ")
-        return response
+    while len(response) != expected_length:
+        response = input(f"That wasn't {expected_length} chars! Try again: ")
+    return response
+
+
+if __name__ == "__main__":
+    main(secret="codes")
